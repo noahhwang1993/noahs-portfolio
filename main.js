7bf2510881d11bb65317f37ca70d09b8b735b00e
noahhwang1993 <ghkdwjddnr1993@gmail.com>
2021-06-15 13:55:59 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -3,12 +3,18 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
 document.addEventListener('scroll', () => {
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }
+
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 // Handle scrolling when tapping on the navbar menu
